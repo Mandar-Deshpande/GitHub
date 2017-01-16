53e72addb6b2b145147f83bc8e1354cea553504a
akashthechamp1 <akash.mane@morningstar.com>
2017-01-16 11:21:51 +0530

    first commit

---
+++ src/main/java/com/morningstar/Calculators/CalcApplication.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.morningstar.Calculators;
+
+/**
+ * @author mdeshpa
+ *
+ */
+public class CalcApplication implements CalcInterface {
+
+	public Double add(double input1, double input2) {
+		return (input1 + input2);
+	}
+
+	public Double subtract(double input1, double input2) {
+		return (input1 - input2);
+	}
+
+	public Double multiply(double input1, double input2) {
+		return (input1 * input2);
+	}
+
+	public Double divide(double input1, double input2) {
+		if (input2 != 0.0) {
+			double x = (input1 / input2);
+			return x;
+		} else {
+			return Double.MIN_VALUE;
+		}
+	}
+
+	public Double expone(double input1) {
+		return (Math.round(Math.exp(input1) * 100.0) / 100.0);
+	}
+
+	public Double nroot(double input1, double input2) {
+		return nthroot(input1, input2, .0001);
+	}
+
+	public double nthroot(double input1, double x, double p) {
+		if (x < 0) {
+			System.err.println("Negative!");
+			return -1;
+		}
+		if (x == 0)
+			return 0;
+		double x1 = x;
+		double x2 = x / input1;
+		while (Math.abs(x1 - x2) > p) {
+			x1 = x2;
+			x2 = ((input1 - 1.0) * x2 + x / Math.pow(x2, input1 - 1.0)) / input1;
+		}
+		return Math.round(x2 * 1000.0) / 1000.0;
+	}
+
+	public double[] qroot(double a, double b, double c) {
+		double[] roots = { 0.0, 0.0 };
+		double d = (b * b) - 4 * a * c;
+		if (d > 0) {
+			roots[0] = (-b + Math.sqrt(d)) / (2 * a);
+			roots[1] = (-b - Math.sqrt(d)) / (2 * a);
+			return roots;
+		} else if (d == 0) {
+			roots[0] = (-b + Math.sqrt(d)) / (2 * a);
+			roots[1] = (-b + Math.sqrt(d)) / (2 * a);
+			return roots;
+		} else {
+			System.out.println("Roots are imaginary");
+			return roots;
+		}
+	}
+}
---
+++ src/main/java/com/morningstar/Calculators/CalcInterface.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.morningstar.Calculators;
+
+/**
+ * @author mdeshpa
+ *
+ */
+public interface CalcInterface {
+
+	public Double add(double input1, double input2);
+
+	public Double subtract(double input1, double input2);
+
+	public Double multiply(double input1, double input2);
+
+	public Double divide(double input1, double input2);
+
+	public Double expone(double input1);
+
+	public Double nroot(double input1, double input2);
+
+	public double[] qroot(double input1, double input2, double input3);
+}
